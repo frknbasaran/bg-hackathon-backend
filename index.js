f6f526c23f06cc04c6e9c09195a8bf6bdbe7b34f
Furkan BASARAN <frknbasaran@gmail.com>
2018-02-17 01:05:03 +0300

    Application folder structure created. Dependencies, layers, automated scripts, etc.

---
+++ index.js
@@ -0,0 +1,30 @@
+/*
+* Load dependencies
+* */
+import Koa from 'koa';
+import Router from 'koa-router';
+import BodyParser from 'koa-bodyparser';
+import DotEnv from 'dotenv';
+import KoaQS from 'koa-qs';
+import KCors from 'koa2-cors';
+
+
+// Create app as a Koa Instance
+const app = new Koa();
+// Create router instance
+const router = new Router();
+// Activate qs module
+KoaQS(app);
+// initalize dotenv
+DotEnv.config();
+
+app
+    // koa cors configuration
+    .use(KCors({origin:'*'}))
+    // inital body parser
+    .use(BodyParser())
+    .use(async (ctx) => {
+        ctx.status = 404;
+        ctx.body = {"success":false, "message":"Invalid api endpoint, please read docs."};
+    })
+    .listen(process.env.PORT);
